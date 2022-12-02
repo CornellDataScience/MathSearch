459f4a9714ed7efc45b4e96edac164851b792294
emerisly <emeralderisly@gmail.com>
2022-12-01 20:14:00 -0500

    flask app

--- ml-model/web/app.py
+++ ml-model/web/app.py
@@ -1,25 +1,35 @@
-"""
-chmod 600 mathsearch-backend.pem
-ssh -i "mathsearch-backend.pem" ubuntu@ec2-18-207-249-45.compute-1.amazonaws.com
-"""
-
-from flask import Flask, flash, redirect, url_for, request
-# from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename
+from flask import Flask, flash, request, redirect, render_template
+import urllib.request
 import os
+from flask import Flask
 
 UPLOAD_FOLDER = '/home/ubuntu/yolov5/input_data'
 
-# ALLOWED_EXTENSIONS = {'pdf'}
-
-# def allowed_file(filename):
-#     return '.' in filename and \
-#         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 app = Flask(__name__)
+app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
-@app.route('/', methods=['GET', 'POST'])
+ALLOWED_EXTENSIONS = set(['pdf'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def hello_world():
+    return 'Warning: go to http://18.207.249.45/upload instead'
+
+
+@app.route('/upload')
+def upload_form():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -27,19 +37,18 @@ def upload_file():
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
+            flash('No file selected for uploading')
             return redirect(request.url)
-        # if file and allowed_file(file.filename):
-        if file:
-            # filename = secure_filename(file.filename)
-            filename = file.filename
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
-    return ''
+            flash('File successfully uploaded')
+            return redirect('/')
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
