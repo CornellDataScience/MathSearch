1f6fec88b1548a16a7008cc0403be1e710d7fadc
emerisly <emeralderisly@gmail.com>
2022-12-03 00:13:42 -0500

    update readme, fix file structure

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
 # MathSearch
 
 A next generation search engine for researchers that supports searching with LaTeX math script.
-
-
-## Run Model
-Deployed with AWS EC2, use url as commands to control flask.\
-ip: 18.207.249.45
-
-Commands:
-1. ```http://18.207.249.45/model?b=<s3_bucket>&o=<s3_object>``` [POST]\
-Post target image and candidate pdf file to ec2.\
-Inputs: s3_bucket, s3_object without _png or _image postfix\
-Sample usage: http://18.207.249.45/model?b=mathsearch-intermediary&o=inputs/2dd62a75-0f22-4121-9bb7-22085cfda77f
-
-2. ```http://18.207.249.45/run```\
-Run machine learning model.
-
-3. ```http://18.207.249.45/coord``` [GET]\
-Get best matching equation page number and bonding box coordinates.
-
-4. ```http://18.207.249.45/upload``` [POST]\
-Backup option to upload input data to ec2.
--- ml-model/web/app.py
+++ ml-model/web/app.py
@@ -1,29 +1,26 @@
 """
 
-Author: Emerald Liu
+@author: Emerald Liu
 
-Deployed flask on ec2
+Deployed with AWS EC2, use url as commands to control flask.\
 ip: 18.207.249.45
-Use url as commands to control flask.
 
 Commands:
-1. http://18.207.249.45/coord [GET]
-Get best matching equation page number and bonding box coordinates.
-Return object see /home/ubuntu/yolov5/main.py
-
-2. http://18.207.249.45/model?b=$s3_bucket$&o=$s3_object$ [POST]
-Post target image and candidate pdf file to ec2.
-Inputs: s3_bucket, s3_object without _png or _image postfix
+1. ```http://18.207.249.45/model?b=<s3_bucket>&o=<s3_object>``` [POST]\
+Post target image and candidate pdf file to ec2.\
+Inputs: s3_bucket, s3_object without _png or _image postfix\
 Sample usage: http://18.207.249.45/model?b=mathsearch-intermediary&o=inputs/2dd62a75-0f22-4121-9bb7-22085cfda77f
 
-3. http://18.207.249.45/upload [POST]
+2. ```http://18.207.249.45/run```\
+Run machine learning model.
+
+3. ```http://18.207.249.45/coord``` [GET]\
+Get best matching equation page number and bonding box coordinates.
+
+4. ```http://18.207.249.45/upload``` [POST]\
 Backup option to upload input data to ec2.
 
-4. http://18.207.249.45/run [POST]
-Run model at /home/ubuntu/yolov5/main.py.
-
 """
-
 
 from werkzeug.utils import secure_filename
 from flask import Flask, flash, request, redirect, render_template
@@ -50,30 +47,33 @@ def hello_world():
 
 # https://www.cs.cornell.edu/~kozen/Papers/daa.pdf
 
+
 @app.route('/coord', methods=['GET'])
 def get_coord():
     f = open('/home/ubuntu/yolov5/ranking/top5.txt', "r")
     return f.read()
 
 
-@app.route('/model', methods=['GET','POST'])
+@app.route('/model', methods=['GET', 'POST'])
 def download():
     s3_bucket = request.args.get('b')
-    s3_object_pdf = request.args.get('o')
-    s3_object_img = s3_object_pdf[:-3]+"img"
+    s3_object = request.args.get('o')
     with open("/home/ubuntu/yolov5/input_info/names.txt", "w") as f:
-        f.write(s3_bucket+"\n"+s3_object_pdf+"\n"+s3_object_img)
-    return s3_bucket+"\n"+s3_object_pdf+"\n"+s3_object_img+"\nPassed data info successfully!"
+        f.write(s3_bucket+"\n"+s3_object)
+    return s3_bucket+"\n"+s3_object+"\nPassed data info successfully!"
 
 
 ALLOWED_EXTENSIONS = set(['pdf'])
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/upload')
 def upload_form():
     return render_template('upload.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -106,7 +106,6 @@ def dynamic_page():
     time.sleep(3)
     return "running model...\n\nVisit:\nhttp://18.207.249.45/coord\nhttp://18.207.249.45/model\n\noptional:\nhttp://18.207.249.45/upload"
 
-if __name__ == "__main__":
-    app.run(debug = True)
 
-    
+if __name__ == "__main__":
+    app.run(debug=True)
