7921b19756c530ddc85940e1fa29a5fe7bb3c60b
Johann Lee <jcl354@cornell.edu>
2022-12-02 23:12:09 -0500

    Siamese done!!

--- ml-model/simese.py
+++ ml-model/simese.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -56,70 +57,10 @@ class SiameseNetwork(nn.Module):
         return out
 
   def forward(self, input1, input2):
-		# It will take the input 'x' until it returns the feature vector called 'out'
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
         return output1, output2
 
-#     return output1, output2
-# class SiameseNetwork(nn.Module):
-#     def __init__(self):
-#         super(SiameseNetwork, self).__init__()
-        
-#         # Setting up the Sequential of CNN Layers
-#         self.cnn1 = nn.Sequential(
-            
-#             nn.Conv2d(1, 96, kernel_size=11,stride=1),
-#             nn.BatchNorm2d(96),
-#             #nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(3, stride=2),
-            
-#             nn.Conv2d(96, 256, kernel_size=5,stride=1,padding=2),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU(inplace=True),
-#             #nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
-#             nn.MaxPool2d(3, stride=2),
-#             nn.Dropout2d(p=0.3),
-
-#             nn.Conv2d(256,384 , kernel_size=3,stride=1,padding=1),
-#             nn.BatchNorm2d(384),
-#             nn.ReLU(inplace=True),
-            
-#             nn.Conv2d(384,256 , kernel_size=3,stride=1,padding=1),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(3, stride=2),
-#             nn.Dropout2d(p=0.3),
-
-#         )
-        
-#         # Defining the fully connected layers
-#         self.fc1 = nn.Sequential(
-#             nn.Linear(30976, 1024),
-#             nn.ReLU(inplace=True),
-#             nn.Dropout2d(p=0.5),
-            
-#             nn.Linear(1024, 128),
-#             nn.ReLU(inplace=True),
-            
-#             nn.Linear(128,2))
-        
-  
-  
-#     def forward_once(self, x):
-#         # Forward pass 
-#         output = self.cnn1(x)
-#         output = output.view(output.size()[0], -1)
-#         output = self.fc1(output)
-#         return output
-
-#     def forward(self, input1, input2):
-#         # forward pass of input 1
-#         output1 = self.forward_once(input1)
-#         # forward pass of input 2
-#         output2 = self.forward_once(input2)
-#         return output1, output2
 
 # from https://github.com/seanbenhur/siamese_net/blob/master/siamese-net/train.py
 # and https://towardsdatascience.com/a-friendly-introduction-to-siamese-networks-85ab17522942
@@ -147,7 +88,8 @@ class SiameseDataset:
     def __init__(self, training_csv=None, training_dir=None, transform=None):
         # used to prepare the labels and images path
         self.train_df = pd.read_csv(training_csv)
-        self.train_df.columns = ["image1", "image2", "label"]
+        # self.train_df = pd.read_csv(training_csv)
+        # self.train_df.columns = ["image1", "image2", "label"]
         self.train_dir = training_dir
         self.transform = transform
 
@@ -197,99 +139,133 @@ class ContrastiveLoss(nn.Module):
         return loss_contrastive
 
 if __name__ == "__main__":
-  training_dir = "simese_data/train"
-  training_csv = "simese_data/train_data.csv"
-  testing_csv = "simese_data/test_data.csv"
-  testing_dir = "simese_data/test"
-  vgg19_model = models.vgg19(pretrained=True)
+    df = pd.DataFrame(columns=['image1', 'image2', 'label'])
+    df_test = pd.DataFrame(columns=['image1', 'image2', 'label'])
+    data_dir = "training_data/Training-Data-MathSearch/output"
+    c = 0
+    for f in os.listdir(data_dir):
 
-  net = SiameseNetwork(vgg19_model)
-#   net = SiameseNetwork()
-  criterion = ContrastiveLoss()
-  optimizer = torch.optim.Adam(net.parameters(), lr=1e-3, weight_decay=0.0005)
+        if c > 20: 
+                df.to_csv("train_data.csv", index=False, header=False)
+                df_test.to_csv("test_data.csv", index=False, header=False)
+                break
+        
+        r1 = os.path.join(data_dir, random.choice(os.listdir(data_dir))) # random directory
+            
+        r0 = random.choice(os.listdir(os.path.join(data_dir, f, "transformed")))
+        r0 = os.path.join(data_dir, f, "transformed", r0)
 
-  # Load the the dataset from raw image folders
-  siamese_dataset = SiameseDataset(
-      training_csv,
-      training_dir,
-      # transform=transforms.ToTensor(),
-      transforms.Compose(
-          [transforms.Resize((105, 105)), transforms.ToTensor()]
-      ),
-  )
+        if c % 10 != 0:
+            # image transform with random original
+            df.loc[len(df.index)] = [r0, os.path.join(r1, 'original_file.jpeg'), 0] 
+            
+            # image transform with its own original
+            df.loc[len(df.index)] = [r0, os.path.join(data_dir, f,"original_file.jpeg"), 1]
+            c += 1
+        
+        if c % 10 == 0:
+            # image transform with random original
+            df_test.loc[len(df.index)] = [r0, os.path.join(r1, 'original_file.jpeg'), 0] 
+            
+            # image transform with its own original
+            df_test.loc[len(df.index)] = [r0, os.path.join(data_dir, f,"original_file.jpeg"), 1]
+            c += 1
 
-  # Viewing the sample of images and to check whether its loading properly
-  vis_dataloader = DataLoader(siamese_dataset, shuffle=True, batch_size=8)
-  dataiter = iter(vis_dataloader)
+    # raise AssertionError
 
+    training_dir = ""
+    training_csv = "train_data.csv"
+    testing_csv = "test_data.csv"
+    testing_dir = ""
+    vgg19_model = models.vgg19(pretrained=True)
 
+    net = SiameseNetwork(vgg19_model)
+    #   net = SiameseNetwork()
+    criterion = ContrastiveLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-3, weight_decay=0.0005)
 
-  train_dataloader = DataLoader(siamese_dataset,
-                          shuffle=True,
-                          num_workers=8,
-                          batch_size=32) 
+    # Load the the dataset from raw image folders
+    siamese_dataset = SiameseDataset(
+        training_csv,
+        training_dir,
+        # transform=transforms.ToTensor(),
+        transforms.Compose(
+            [transforms.Resize((105, 105)), transforms.ToTensor()]
+        ),
+    )
 
-  test_dataset = SiameseDataset(training_csv=testing_csv,training_dir=testing_dir,
-                                          transform=transforms.Compose([transforms.Resize((105,105)),
-                                                                        transforms.ToTensor()
-                                                                        ])
-                                        )
-
-  test_dataloader = DataLoader(test_dataset,num_workers=6,batch_size=1,shuffle=True)
-
-  #train the model
-  def train():
-      loss=[] 
-      counter=[]
-      iteration_number = 0
-
-      k = 0
-      for epoch in range(1, 2):
-          for i, data in enumerate(test_dataloader,0):
-              k += 1
-              if k % 10 == 0: print(k)
-          # for i, data in enumerate(train_dataloader,0):
-              img0, img1 , label = data
-              # img0, img1 , label = img0.cuda(), img1.cuda() , label.cuda()
-              optimizer.zero_grad()
-              output1,output2 = net(img0,img1)
-              loss_contrastive = criterion(output1,output2,label)
-              loss_contrastive.backward()
-              optimizer.step()    
-          print("Epoch {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
-          iteration_number += 10
-          counter.append(iteration_number)
-          loss.append(loss_contrastive.item())
-      show_plot(counter, loss)   
-      return net
+    # Viewing the sample of images and to check whether its loading properly
+    vis_dataloader = DataLoader(siamese_dataset, shuffle=True, batch_size=8)
+    dataiter = iter(vis_dataloader)
 
 
-  #set the device to cuda
-  device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 
-#   device = torch.device('cpu')
-  model = train()
-  torch.save(model.state_dict(), "model.pt")
-  print("Model Saved Successfully") 
+    train_dataloader = DataLoader(siamese_dataset,
+                            shuffle=True,
+                            num_workers=8,
+                            batch_size=32) 
+
+    test_dataset = SiameseDataset(training_csv=testing_csv,training_dir=training_dir,
+                                            transform=transforms.Compose([transforms.Resize((105,105)),
+                                                                            transforms.ToTensor()
+                                                                            ])
+                                            )
+
+    test_dataloader = DataLoader(test_dataset,num_workers=6,batch_size=1,shuffle=True)
+
+    #train the model
+    def train():
+        loss=[] 
+        counter=[]
+        iteration_number = 0
+
+        k = 0
+        for epoch in range(1, 2):
+            for i, data in enumerate(test_dataloader,0):
+                k += 1
+                if k % 10 == 0: print(k)
+            # for i, data in enumerate(train_dataloader,0):
+                img0, img1 , label = data
+                # img0, img1 , label = img0.cuda(), img1.cuda() , label.cuda()
+                optimizer.zero_grad()
+                output1,output2 = net(img0,img1)
+                loss_contrastive = criterion(output1,output2,label)
+                loss_contrastive.backward()
+                optimizer.step()    
+            print("Epoch {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
+            iteration_number += 10
+            counter.append(iteration_number)
+            loss.append(loss_contrastive.item())
+        show_plot(counter, loss)   
+        return net
 
 
-  # #test the network
-  # count=0
-  # for i, data in enumerate(test_dataloader,0): 
-  #   x0, x1 , label = data
-  #   concat = torch.cat((x0,x1),0)
-  #   output1,output2 = model(x0.to(device),x1.to(device))
+    #set the device to cuda
+    device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 
-  #   eucledian_distance = F.pairwise_distance(output1, output2)
-      
-  #   if label==torch.FloatTensor([[0]]):
-  #     label="Original Pair Of Signature"
-  #   else:
-  #     label="Forged Pair Of Signature"
-      
-  #   imshow(torchvision.utils.make_grid(concat))
-  #   print("Predicted Eucledian Distance:-",eucledian_distance.item())
-  #   print("Actual Label:-",label)
-  #   count=count+1
-  #   if count ==10:
-  #     break
+    #   device = torch.device('cpu')
+    model = train()
+    torch.save(model.state_dict(), "model.pt")
+    print("Model Saved Successfully") 
+
+
+    #test the network
+    count=0
+    for i, data in enumerate(test_dataloader,0): 
+        x0, x1 , label = data
+        concat = torch.cat((x0,x1),0)
+        output1,output2 = model(x0.to(device),x1.to(device))
+
+        eucledian_distance = F.pairwise_distance(output1, output2)
+        
+        if label==torch.FloatTensor([[0]]):
+            label="Original Pair Of Signature"
+        else:
+            label="Forged Pair Of Signature"
+        
+        imshow(torchvision.utils.make_grid(concat))
+        print("Predicted Eucledian Distance:-",eucledian_distance.item())
+        print("Actual Label:-",label)
+        count=count+1
+        if count ==10:
+            break
