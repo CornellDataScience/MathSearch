6d76d5abcdba3317106a2a4d2278dcdf79044bb6
Jason Zheng <jasonz4200@gmail.com>
2023-05-03 18:08:35 -0400

    Finish api!

--- front-end/mathsearch/src/components/pages/Home.js
+++ front-end/mathsearch/src/components/pages/Home.js
@@ -11,16 +11,36 @@ import './Home.css'
 function Home() {
   const navigate = useNavigate()
   const redirect = async () => {
-    const url = "http://mathsearch.org/api/result"
+    // Test code
+    // try {
+    const url = "http://mathsearch.org/api/responsetest";
     const response = await fetch(url);
-    const json = await response.json();
+    const pdfBinary = await response.blob()
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://mozilla.github.io/pdf.js/web/viewer.html?file=${url}`;
+    document.body.appendChild(iframe);
+    // } catch (err) {
+    //   console.log(err)
+    // }
+
+    // Current code
+    // const url = "http://mathsearch.org/api/result"
+    // const response = await fetch(url);
+    // const json = await response.json();
     // console.log("hello")
     // console.log(json)
-    navigate('/results',{state:{pdf:pdf, pages:[1, 2, 3]}})
+    navigate('/results',{state:{pdf:pdfBinary, pages:[1, 2, 3]}})
+  }
+
+  const test_api = async () => {
+    const url = "http://mathsearch.org/api/test"
+    const response = await fetch(url);
+    console.log(await response.text());
   }
 
   const handleClick = () => {
     redirect()
+    // test_api()
   }
 
   return (
