5a84007523be7387dea17af9e0b5c4bf996df809
Jason Zheng <jasonz4200@gmail.com>
2023-10-28 10:51:54 -0400

    Move API request from Home to Results page

--- front-end/mathsearch/src/components/pages/Home.js
+++ front-end/mathsearch/src/components/pages/Home.js
@@ -10,72 +10,62 @@ import '../../App.css';
 import './Home.css'
 
 function Home() {
-  const [loading, setLoading] = useState(false)
+  // const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
-  const redirect = async () => {
-    // Test code
-    // try {
-    const url = "http://mathsearch.org/api/response_pdf";
-    const response = await fetch(url);
-    // console.log("COOL")
-    // console.log(response)
-    const pdfBinary = await response.blob()
+  // const redirect = async () => {
+  //   // Test code
+  //   // try {
+  //   const url = "http://mathsearch.org/api/response_pdf";
+  //   const response = await fetch(url);
+  //   // console.log("COOL")
+  //   // console.log(response)
+  //   const pdfBinary = await response.blob()
 
-    const url2 = "http://mathsearch.org/api/response_pages"
-    const response2 = await fetch(url2);
-    const pages = await response2.json()
-    // console.log(pages)
-    // const iframe = document.createElement('iframe');
-    // iframe.src = `https://mozilla.github.io/pdf.js/web/viewer.html?file=${url}`;
-    // document.body.appendChild(iframe);
-    // } catch (err) {
-    //   console.log(err)
-    // }
+  //   const url2 = "http://mathsearch.org/api/response_pages"
+  //   const response2 = await fetch(url2);
+  //   const pages = await response2.json()
+  //   // console.log(pages)
+  //   // const iframe = document.createElement('iframe');
+  //   // iframe.src = `https://mozilla.github.io/pdf.js/web/viewer.html?file=${url}`;
+  //   // document.body.appendChild(iframe);
+  //   // } catch (err) {
+  //   //   console.log(err)
+  //   // }
 
-    // Current code
-    // const url = "http://mathsearch.org/api/result"
-    // const response = await fetch(url);
-    // const json = await response.json();
-    // console.log("hello")
-    // console.log(json)
-    navigate('/results/request_id_here', { state: { pdf: pdfBinary, pages: pages } })
-  }
+  //   // Current code
+  //   // const url = "http://mathsearch.org/api/result"
+  //   // const response = await fetch(url);
+  //   // const json = await response.json();
+  //   // console.log("hello")
+  //   // console.log(json)
+  //   navigate('/results/request_id_here', { state: { pdf: pdfBinary, pages: pages } })
+  // }
 
-  const test_api = async () => {
-    const url = "http://mathsearch.org/api/test"
-    const response = await fetch(url);
-    console.log(await response.text());
-  }
+  // const test_api = async () => {
+  //   const url = "http://mathsearch.org/api/test"
+  //   const response = await fetch(url);
+  //   console.log(await response.text());
+  // }
 
   const handleClick = async () => {
-    setLoading(true)
+    // setLoading(true)
     // await new Promise(resolve => setTimeout(resolve, 5000));
-    redirect()
+    // redirect()
+    navigate('/results/request_id_here')
     // test_api()
   }
 
   return (
     <>
-      {loading ?
-        <div class="page">
-          <div class="center">
-            <div class="loader"></div>
-          </div>
-
+      <NavBar />
+      <div className="home-container">
+        <div className="home-content">
+          <h1 className="title">MathSearch</h1>
+          <LaTeXInput />
+          {/* <UploadPDFToS3WithNativeSdk /> */}
+          <button onClick={handleClick}>Redirect to results</button>
         </div>
-        :
-        <div>
-          <NavBar />
-          <div className="home-container">
-            <div className="home-content">
-              <h1 className="title">MathSearch</h1>
-              <LaTeXInput />
-              {/* <UploadPDFToS3WithNativeSdk /> */}
-              <button onClick={handleClick}>Redirect to results</button>
-            </div>
-          </div>
-        </div>
-      }
+      </div>
     </>
   );
 }
