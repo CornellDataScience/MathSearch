b2b6263b9aa237f860557cd76233c6e045693f8e
Jason Zheng <jasonz4200@gmail.com>
2023-11-06 18:45:43 -0500

    Add contributors

--- front-end/mathsearch/src/components/pages/About.js
+++ front-end/mathsearch/src/components/pages/About.js
@@ -14,7 +14,37 @@ function About() {
             <div style={{ backgroundColor: "#eeeeee", minHeight: "100vh" }}>
                 <NavBar />
                 <div className="home-container">
-                    Hello
+                    <p>Current team members:</p>
+                    <ul>
+                        <li>Andrea Siby</li>
+                        <li>Cade Jin</li>
+                        <li>Deniz BT</li>
+                        <li>Emerald Liu</li>
+                        <li>Jason Zheng</li>
+                        <li>Jerry Chen</li>
+                        <li>Katie Zelvin</li>
+                        <li>Pun Chalxanien</li>
+                        <li>Travis Zhang</li>
+                    </ul>
+                    <p>Past team members: (SP23)</p>
+                    <ul>
+                        <li>Alice Um</li>
+                        <li>Darren Key</li>
+                        <li>Johann Lee</li>
+                    </ul>
+                    <p>Past team members: (FA22)</p>
+                    <ul>
+                        <li>Alexander Wang</li>
+                        <li>Derek Lee</li>
+                        <li>Felix Hohne</li>
+                        <li>Kaitlyn Chen</li>
+                        <li>Laura Gong</li>
+                        <li>Mason Bulling</li>
+                        <li>Ronin Sharma</li>
+                        <li>Ryan Lee</li>
+                        <li>Varun Gande</li>
+                        <li>Vivian Chen</li>
+                    </ul>
                 </div>
             </div>
         </>
