91d2c319592fdfd9d7ad6a5d7720adb249a6e963
Ubuntu <ubuntu@ip-172-31-70-159.ec2.internal>
2023-11-18 16:43:14 +0000

    Deleted test.py and added back index.js

--- front-end/web/test.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-session = boto3.Session(profile_name='default')
-s3_session = session.resource('s3')
-bucket = s3_session.Bucket('mathsearch-intermediary')
-for obj in bucket.objects.all():
-   print(obj.key)
