f0ef55bf41d85247211c5408990bb25e112b220f
Jason Zheng <jasonz4200@gmail.com>
2023-12-02 10:49:25 -0500

    Fix About page

--- front-end/mathsearch/src/components/pages/About.js
+++ front-end/mathsearch/src/components/pages/About.js
@@ -1,54 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import NavBar from '../NavBar.js';
+import NavBar from "../NavBar.js";
 
-import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.css";
 
-import '../../App.css';
-import './Home.css'
+import "../../App.css";
+import "./Home.css";
 
 function About() {
-    return (
-        <>
-            <div style={{ backgroundColor: "#eeeeee", minHeight: "100vh" }}>
-                <NavBar />
-                <div className="home-container">
-                    <p>Current team members:</p>
-                    <ul>
-                        <li>Andrea Siby</li>
-                        <li>Cade Jin</li>
-                        <li>Deniz BT</li>
-                        <li>Emerald Liu</li>
-                        <li>Jason Zheng</li>
-                        <li>Jerry Chen</li>
-                        <li>Katie Zelvin</li>
-                        <li>Pun Chalxanien</li>
-                        <li>Travis Zhang</li>
-                    </ul>
-                    <p>Past team members: (SP23)</p>
-                    <ul>
-                        <li>Alice Um</li>
-                        <li>Darren Key</li>
-                        <li>Johann Lee</li>
-                    </ul>
-                    <p>Past team members: (FA22)</p>
-                    <ul>
-                        <li>Alexander Wang</li>
-                        <li>Derek Lee</li>
-                        <li>Felix Hohne</li>
-                        <li>Kaitlyn Chen</li>
-                        <li>Laura Gong</li>
-                        <li>Mason Bulling</li>
-                        <li>Ronin Sharma</li>
-                        <li>Ryan Lee</li>
-                        <li>Varun Gande</li>
-                        <li>Vivian Chen</li>
-                    </ul>
-                </div>
+  return (
+    <>
+      <div style={{ backgroundColor: "#eeeeee", minHeight: "100vh" }}>
+        <div style={{ height: "100vh" }}>
+          <NavBar />
+          <div
+            style={{ width: "100%", display: "flex", justifyContent: "center", marginTop: "50px" }}
+          >
+            <div>
+              <p>Current team members:</p>
+              <ul>
+                <li>Andrea Siby</li>
+                <li>Cade Jin</li>
+                <li>Deniz BT</li>
+                <li>Emerald Liu</li>
+                <li>Jason Zheng</li>
+                <li>Jerry Chen</li>
+                <li>Katie Zelvin</li>
+                <li>Pun Chalxanien</li>
+                <li>Travis Zhang</li>
+              </ul>
+              <p>Past team members: (SP23)</p>
+              <ul>
+                <li>Alice Um</li>
+                <li>Darren Key</li>
+                <li>Johann Lee</li>
+              </ul>
+              <p>Past team members: (FA22)</p>
+              <ul>
+                <li>Alexander Wang</li>
+                <li>Derek Lee</li>
+                <li>Felix Hohne</li>
+                <li>Kaitlyn Chen</li>
+                <li>Laura Gong</li>
+                <li>Mason Bulling</li>
+                <li>Ronin Sharma</li>
+                <li>Ryan Lee</li>
+                <li>Varun Gande</li>
+                <li>Vivian Chen</li>
+              </ul>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
