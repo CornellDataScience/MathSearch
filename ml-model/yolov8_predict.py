f297a034446690967bf7d0921fbcc5387ec97250
Ubuntu <ubuntu@ip-172-31-79-229.ec2.internal>
2024-03-03 19:54:24 +0000

    Create cropped image datasets, run Yolov8

---
+++ ml-model/yolov8_predict.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import os
+
+# Load a model
+model = YOLO('runs/detect/train2/weights/best.pt')  # load custom equation model
+print("Loaded YOLO model!")
+img_directory = 'datasets/eqn-images-dataset/images/train'
+
+# Predict with the model
+img_lst = [os.path.join(img_directory, img) for img in os.listdir(img_directory)] 
+results = model(img_lst)
+print("Ran YOLO model on img_dir!")
+
+# process results, save cropped images
+for r in results:
+  r.save_crop('cropped_data/')
+print("Saved crops!")
+
