8fc7207706262845760691953bf475ccc978eb5e
PeterFavero <peterfavero10@gmail.com>
2024-03-21 18:36:14 -0400

    update requester

--- ml-model/mathpix/api_requester.py
+++ ml-model/mathpix/api_requester.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import io
 
 #Hardcoded CDS account response headers (placeholders for now)
 headers = {
@@ -9,7 +10,6 @@ headers = {
 
 mathpix_url = "https://api.mathpix.com/v3/text"
 
-mathpix_url = "https://api.mathpix.com/v3/text"
 
 def s3_image_to_latex(image_s3_url) :
     
@@ -29,8 +29,8 @@ def s3_image_to_latex(image_s3_url) :
     # Check if the request was successful
     if response.status_code == 200:
         response_data = response.json()
-        print(json.dumps(response_data, indent=4, sort_keys=True))  # Print formatted JSON response
-        print()
+        #print(json.dumps(response_data, indent=4, sort_keys=True))  # Print formatted JSON response
+        #print()
         return response_data.get("latex_styled", "")  # Get the LaTeX representation from the response, safely access the key
     else:
         print("Failed to get LaTeX. Status code:", response.status_code)
@@ -39,7 +39,6 @@ def s3_image_to_latex(image_s3_url) :
 def jpg_image_to_latex(image_jpg_path) :
     
     #Declare api request payload (refer to https://docs.mathpix.com/?python#introduction for description)
-    #s3 url version: 
     data = {
         "formats": ["latex_styled"], 
         "rm_fonts": True, 
@@ -49,26 +48,54 @@ def jpg_image_to_latex(image_jpg_path) :
 
     # It's better to use the with statement when dealing with file operations
     response = requests.post("https://api.mathpix.com/v3/text",
-                              files={"file": open(image_jpg_path,"rb")},
+                              files={"file": open(image_jpg_path,"rb").read()},
                               data={"options_json": json.dumps(data)},
                               headers=headers)
+    
 
     # Check if the request was successful
     if response.status_code == 200:
         response_data = response.json()
-        print(json.dumps(response_data, indent=4, sort_keys=True))  # Print formatted JSON response
-        print()
+        #print(json.dumps(response_data, indent=4, sort_keys=True))  # Print formatted JSON response
+        #print()
+        return response_data.get("latex_styled", "")  # Get the LaTeX representation from the response, safely access the key
+    else:
+        print("Failed to get LaTeX. Status code:", response.status_code)
+        return ""
+    
+def bytes_arry_to_latex(bytes_array) :
+    
+    #Declare api request payload (refer to https://docs.mathpix.com/?python#introduction for description)
+    data = {
+        "formats": ["latex_styled"], 
+        "rm_fonts": True, 
+        "rm_spaces": False,
+        "idiomatic_braces": True
+    }
+
+    # It's better to use the with statement when dealing with file operations
+    response = requests.post("https://api.mathpix.com/v3/text",
+                              files={"file": io.BufferedReader(io.BytesIO(bytes_array))},
+                              data={"options_json": json.dumps(data)},
+                              headers=headers)
+    
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        response_data = response.json()
+        #print(json.dumps(response_data, indent=4, sort_keys=True))  # Print formatted JSON response
+        #print()
         return response_data.get("latex_styled", "")  # Get the LaTeX representation from the response, safely access the key
     else:
         print("Failed to get LaTeX. Status code:", response.status_code)
         return ""
 
-#print(jpg_image_to_latex('big-eqn/im.jpg14.jpg'))
 print()
-#print(jpg_image_to_latex('big-eqn/im.jpg17.jpg'))
+print(jpg_image_to_latex('big-eqn/im.jpg14.jpg'))
 print()
-#print(jpg_image_to_latex('big-eqn/im.jpg714.jpg')) # This one is hard, the OCR getting this seems really impressive
+print(jpg_image_to_latex('big-eqn/im.jpg17.jpg'))
+print()
+print(jpg_image_to_latex('big-eqn/im.jpg714.jpg')) # This one is hard, the OCR getting this seems really impressive
 print()
 print(jpg_image_to_latex('big-eqn/im.jpg728.jpg')) # matrix
-
-    
+print()
